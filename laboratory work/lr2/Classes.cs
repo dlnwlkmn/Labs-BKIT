94d71f845af518561662341342151ad4559b58b9
Alex Savelyev <lesh77906@gmail.com>
2019-10-18 12:39:23 +0300

    В общем решении созданн проект второй лабораторной работы. Перемещены файлы Program.cs и  Classes.cs сделанной некоректно лабораторной работы для дальнейшего исправления.

---
+++ laboratory work/lr2/Classes.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2_1
+{
+    interface IPrint
+    {
+        void Print();
+    }
+    abstract class GeomFigure
+    {
+        string type;
+        public string Type
+        {
+            protected set
+            {
+                this.type = value;
+            }
+            get
+            {
+                return this.type;
+            }
+        }
+
+        public abstract double Square();
+        public override string ToString()
+        {
+            return this.type + " площадью " + this.Square();
+        }
+    }
+    class Rectangle : GeomFigure, IPrint
+    {
+        double width;
+        double height;
+        public Rectangle(double w, double h) //Главные конструктор класса
+        {
+            this.width = w;
+            this.height = h;
+            this.Type = "Прямоугольник";
+        }
+        public override double Square()
+        {
+            return this.width * this.height;
+        }
+        public void Print()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(this.ToString());
+            Console.ResetColor();
+        }
+    }
+    class Squad : Rectangle
+    {
+        public Squad(double a)
+            : base(a, a)
+        {
+            this.Type = "Квадрат";
+        }
+    }
+    class Circle : GeomFigure, IPrint
+    {
+        double radius;
+        public Circle(double r)
+        {
+            this.radius = r;
+            this.Type = "Круг";
+        }
+        public override double Square()
+        {
+            return System.Math.PI * this.radius * this.radius;
+        }
+        public void Print()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(this.ToString());
+            Console.ResetColor();
+        }
+
+    }
+}
---
+++ laboratory work/lr2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2_1
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Студент:\tСавельев Алексей");
+            Console.WriteLine("Группа:\t\tИУ5-34Б");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Работа с классами на C#");
+            Console.ResetColor();
+            Rectangle rect;
+            Circle circ;
+            Squad squad;
+            while (true)
+            {
+
+                switch(menu())
+                {
+                    case 1:
+                        double width = toDoubleCase("\nВведите ширину:");
+                        double height = toDoubleCase("Введите высоту:");
+                        rect = new Rectangle(width, height);
+                        rect.Print();
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        double sideLength = toDoubleCase("\nВведите сторону квадрата:");
+                        squad = new Squad(sideLength);
+                        squad.Print();
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        double radius = toDoubleCase("\nВведите радиус:");
+                        circ = new Circle(radius);
+                        circ.Print();
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nКонец...");
+                        Console.ReadKey();
+                        return 0;
+                }
+            }
+        }
+
+        static int menu()
+        {
+            int choice;
+            string str;
+            Console.WriteLine(
+                "\nВыберитеГеометрическую фигуру:\n" +
+                "1) Прямоугольник\n" +
+                "2) Квадрат\n" +
+                "3) Круг\n" +
+                "4) Завершить"
+                );
+            str = Console.ReadLine();
+            int.TryParse(str, out choice);
+            return choice;
+        }
+
+        static double toDoubleCase(string cout)
+        {
+            double doubleCase = 0;
+            string inputCase;
+            bool stop = false;
+
+            do
+            {
+                Console.WriteLine(cout);
+                inputCase = Console.ReadLine();
+
+                try
+                {
+                    doubleCase = double.Parse(inputCase);
+                    //stop = double.TryParse(inputCase, out doubleCase);
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nВы ввели не число: " + e.Message);
+                    Console.WriteLine("\nПодробное описание ошибки: ");
+                    Console.ResetColor();
+                    Console.WriteLine(e.StackTrace + "\n");
+                }
+                if (doubleCase != 0) { stop = true; }
+
+            } while (!stop);
+
+            return doubleCase;
+        }
+    }
+}
