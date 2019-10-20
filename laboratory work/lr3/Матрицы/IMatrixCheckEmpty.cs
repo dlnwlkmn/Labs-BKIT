d620c92122f602ae22c282907c8fc959bd2643dc
Alex Savelyev <lesh77906@gmail.com>
2019-10-20 21:07:23 +0300

    Разработан интерфейс проверки пустого элемента матрицы

---
+++ laboratory work/lr3/Матрицы/IMatrixCheckEmpty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr3
+{
+    public interface IMatrixCheckEmpty<T>
+    {
+        // возврат пустого элемента
+        T getEmptyElement();
+
+        // проверка, пумтой ли элемент
+        bool checkEmptyElement(T element);
+    }
+}
