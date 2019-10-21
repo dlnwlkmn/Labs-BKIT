e3787967c92bdc19fcdbb594e71fc33ca5b12743
Alex Savelyev <lesh77906@gmail.com>
2019-10-21 23:26:58 +0300

    разработан класс элемента списка SimpleList

---
+++ laboratory work/lr3/SimpleListItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr3
+{
+    class SimpleListItem<T>
+    {
+        public T data { get; set; }
+
+        // следующий элемент
+        public SimpleListItem<T> next{ get; set; }
+
+        public SimpleListItem(T parametr)
+        {
+            this.data = parametr;
+        }
+
+    }
+}
