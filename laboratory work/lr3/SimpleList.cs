f0e590d62074a38e2a68cadd5c18949e0c50801c
Alex Savelyev <lesh77906@gmail.com>
2019-10-21 23:34:24 +0300

    Разработка класса списка SimpleList. Метод "Кол-во элементов"

---
+++ laboratory work/lr3/SimpleList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr3
+{
+    class SimpleList<T>
+    {
+        // первый элемент списка
+        protected SimpleListItem<T> first = null;
+
+        // последний элемент списка
+        protected SimpleListItem<T> second = null;
+
+        int _count;
+        public int count
+        {
+            get { return _count; }
+            protected set { _count = value; }
+        }
+
+        
+    }
+}
