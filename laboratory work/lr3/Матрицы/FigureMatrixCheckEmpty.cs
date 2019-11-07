958061fc3b24ed98d0fc9513e86ffba3332fa483
Alexey <lesh77906@gmail.com>
2019-11-07 09:57:35 +0300

    Класс FigureMatrixCheckEmpty реальзован в отдельном файле для доступности в lr3

---
+++ laboratory work/lr3/Матрицы/FigureMatrixCheckEmpty.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using laba2_1;
+
+namespace lr3
+{
+    public class FigureMatrixCheckEmpty : IMatrixCheckEmpty<GeomFigure>
+    {
+
+        public GeomFigure getEmptyElement()
+        {
+            return null;
+        }
+
+
+        public bool checkEmptyElement(GeomFigure element)
+        {
+            bool Result = false;
+            if (element == null)
+            {
+                Result = true;
+            }
+            return Result;
+        }
+    }
+}
