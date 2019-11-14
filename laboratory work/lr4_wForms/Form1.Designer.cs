bc1a2704135e806539f2fbf68ea03767d4d2e31e
Alexey <lesh77906@gmail.com>
2019-11-14 15:20:32 +0300

    Реальзована логика кнопки просмотра текста.

--- laboratory work/lr4_wForms/Form1.Designer.cs
+++ laboratory work/lr4_wForms/Form1.Designer.cs
@@ -40,14 +40,15 @@
             this.label5 = new System.Windows.Forms.Label();
             this.textBoxExactTime = new System.Windows.Forms.TextBox();
             this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.textButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // loadupFileButton
             // 
-            this.loadupFileButton.Location = new System.Drawing.Point(162, 17);
-            this.loadupFileButton.Margin = new System.Windows.Forms.Padding(2);
+            this.loadupFileButton.Location = new System.Drawing.Point(324, 33);
+            this.loadupFileButton.Margin = new System.Windows.Forms.Padding(4);
             this.loadupFileButton.Name = "loadupFileButton";
-            this.loadupFileButton.Size = new System.Drawing.Size(54, 24);
+            this.loadupFileButton.Size = new System.Drawing.Size(108, 46);
             this.loadupFileButton.TabIndex = 0;
             this.loadupFileButton.Text = "Обзор";
             this.loadupFileButton.UseVisualStyleBackColor = true;
@@ -56,73 +57,75 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 22);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(32, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(144, 13);
+            this.label1.Size = new System.Drawing.Size(285, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "Выберите файл для чтения";
             // 
             // textBoxFileReadTime
             // 
             this.textBoxFileReadTime.BackColor = System.Drawing.SystemColors.Info;
-            this.textBoxFileReadTime.Location = new System.Drawing.Point(210, 66);
-            this.textBoxFileReadTime.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxFileReadTime.Location = new System.Drawing.Point(420, 127);
+            this.textBoxFileReadTime.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxFileReadTime.Name = "textBoxFileReadTime";
-            this.textBoxFileReadTime.Size = new System.Drawing.Size(341, 20);
+            this.textBoxFileReadTime.Size = new System.Drawing.Size(678, 31);
             this.textBoxFileReadTime.TabIndex = 2;
             // 
             // textBoxFileReadCount
             // 
             this.textBoxFileReadCount.BackColor = System.Drawing.SystemColors.Info;
-            this.textBoxFileReadCount.Location = new System.Drawing.Point(210, 90);
-            this.textBoxFileReadCount.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxFileReadCount.Location = new System.Drawing.Point(420, 173);
+            this.textBoxFileReadCount.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxFileReadCount.Name = "textBoxFileReadCount";
-            this.textBoxFileReadCount.Size = new System.Drawing.Size(341, 20);
+            this.textBoxFileReadCount.Size = new System.Drawing.Size(516, 31);
             this.textBoxFileReadCount.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 69);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(32, 133);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.Size = new System.Drawing.Size(227, 25);
             this.label2.TabIndex = 4;
             this.label2.Text = "Время чтения файла:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 93);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(32, 179);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(181, 13);
+            this.label3.Size = new System.Drawing.Size(354, 25);
             this.label3.TabIndex = 5;
             this.label3.Text = "Зафиксировано уникальных слов:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(16, 133);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(32, 256);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(132, 13);
+            this.label4.Size = new System.Drawing.Size(256, 25);
             this.label4.TabIndex = 6;
             this.label4.Text = "Введите искомое слово:";
             // 
             // searchWord
             // 
-            this.searchWord.Location = new System.Drawing.Point(210, 128);
+            this.searchWord.Location = new System.Drawing.Point(420, 246);
+            this.searchWord.Margin = new System.Windows.Forms.Padding(6);
             this.searchWord.Name = "searchWord";
-            this.searchWord.Size = new System.Drawing.Size(260, 20);
+            this.searchWord.Size = new System.Drawing.Size(516, 31);
             this.searchWord.TabIndex = 7;
             // 
             // searchButton
             // 
-            this.searchButton.Location = new System.Drawing.Point(476, 126);
+            this.searchButton.Location = new System.Drawing.Point(948, 240);
+            this.searchButton.Margin = new System.Windows.Forms.Padding(6);
             this.searchButton.Name = "searchButton";
-            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.Size = new System.Drawing.Size(150, 44);
             this.searchButton.TabIndex = 8;
             this.searchButton.Text = "Поиск";
             this.searchButton.UseVisualStyleBackColor = true;
@@ -131,36 +134,50 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(16, 156);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(32, 300);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(115, 13);
+            this.label5.Size = new System.Drawing.Size(222, 25);
             this.label5.TabIndex = 10;
             this.label5.Text = "Время поиска слова:";
             // 
             // textBoxExactTime
             // 
             this.textBoxExactTime.BackColor = System.Drawing.SystemColors.Info;
-            this.textBoxExactTime.Location = new System.Drawing.Point(210, 153);
-            this.textBoxExactTime.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxExactTime.Location = new System.Drawing.Point(420, 294);
+            this.textBoxExactTime.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxExactTime.Name = "textBoxExactTime";
-            this.textBoxExactTime.Size = new System.Drawing.Size(341, 20);
+            this.textBoxExactTime.Size = new System.Drawing.Size(678, 31);
             this.textBoxExactTime.TabIndex = 9;
             // 
             // listBoxResult
             // 
             this.listBoxResult.FormattingEnabled = true;
-            this.listBoxResult.Location = new System.Drawing.Point(19, 193);
+            this.listBoxResult.ItemHeight = 25;
+            this.listBoxResult.Location = new System.Drawing.Point(38, 371);
+            this.listBoxResult.Margin = new System.Windows.Forms.Padding(6);
             this.listBoxResult.Name = "listBoxResult";
-            this.listBoxResult.Size = new System.Drawing.Size(532, 160);
+            this.listBoxResult.Size = new System.Drawing.Size(1060, 304);
             this.listBoxResult.TabIndex = 11;
+            // 
+            // textButton
+            // 
+            this.textButton.Location = new System.Drawing.Point(948, 169);
+            this.textButton.Margin = new System.Windows.Forms.Padding(6);
+            this.textButton.Name = "textButton";
+            this.textButton.Size = new System.Drawing.Size(150, 44);
+            this.textButton.TabIndex = 12;
+            this.textButton.Text = "Текст";
+            this.textButton.UseVisualStyleBackColor = true;
+            this.textButton.Click += new System.EventHandler(this.textButton_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(569, 464);
+            this.ClientSize = new System.Drawing.Size(1138, 892);
+            this.Controls.Add(this.textButton);
             this.Controls.Add(this.listBoxResult);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBoxExactTime);
@@ -173,7 +190,7 @@
             this.Controls.Add(this.textBoxFileReadTime);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.loadupFileButton);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Чтение файла, поиск слов";
             this.ResumeLayout(false);
@@ -195,6 +212,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBoxExactTime;
         private System.Windows.Forms.ListBox listBoxResult;
+        private System.Windows.Forms.Button textButton;
     }
 }
 
