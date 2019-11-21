b334cfd94f7ef83d334dbfd78e3dec74545cbe0a
Alexey <lesh77906@gmail.com>
2019-11-21 21:39:38 +0300

    Реализовано вычисление расстояния Левенштейна

--- laboratory work/lr5/LevDist.cs
+++ laboratory work/lr5/LevDist.cs
@@ -10,12 +10,11 @@ namespace lr5
     {
         public static int Distance(string str1, string str2)
         {
-            if (str1 == null || str2 == null) return -1;
 
             int length1 = str1.Length;
             int length2 = str2.Length;
 
-            if (length1 == 0 && length2 == 0) return 0;
+            if ((length1 == 0) && (length2 == 0)) return -1;
             if (length1 == 0) return length2;
             if (length2 == 0) return length1;
 
@@ -27,10 +26,24 @@ namespace lr5
             for (int i = 0; i <= length1; i++) matrix[i, 0] = i;
             for (int j = 0; j <= length2; j++) matrix[0, j] = j;
 
-            for(int i = 0; i < length1; i++)
+            for (int i = 1; i <= length1; i++)
             {
-                for(int j = 0; j < length2; j++)
+                for (int j = 1; j <= length2; j++)
                 {
+                    int symbEqual = ((str1.Substring(i - 1, 1) == str2.Substring(j - 1, 1)) ? 0 : 1);
+
+                    int insert = matrix[i, j - 1] + 1; // добавление
+                    int delete = matrix[i - 1, j] + 1; // удаление
+                    int subst = matrix[i - 1, j - 1] + symbEqual; // замена
+
+                    matrix[i, j] = Math.Min(Math.Min(insert, delete), subst);
+
+                    if ((i > 1) && (j > 1) &&
+                        (strUpp1.Substring(i - 1, 1) == strUpp2.Substring(j - 2, 1)) &&
+                        (strUpp1.Substring(i - 2, 1) == strUpp2.Substring(j - 1, 1)))
+                    {
+                        matrix[i, j] = Math.Min(matrix[i, j], matrix[i - 2, j - 2] + symbEqual);
+                    }
 
                 }
             }
