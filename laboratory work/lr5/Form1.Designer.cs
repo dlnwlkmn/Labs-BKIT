5ec027d9f74e08b60467dd5659a6f573f7fb21eb
Alexey <lesh77906@gmail.com>
2019-11-21 22:16:55 +0300

    Реализован учет максимального (Вводится по выбору, по умолчанию 0) расстояния и проверка текущего вычисления

--- laboratory work/lr5/Form1.Designer.cs
+++ laboratory work/lr5/Form1.Designer.cs
@@ -36,6 +36,8 @@
             this.label3 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -46,7 +48,6 @@
             this.label1.Size = new System.Drawing.Size(188, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Исходная строка:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
@@ -73,7 +74,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(38, 152);
+            this.button1.Location = new System.Drawing.Point(38, 204);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(183, 37);
             this.button1.TabIndex = 4;
@@ -84,17 +85,16 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(252, 158);
+            this.label3.Location = new System.Drawing.Point(252, 210);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(179, 25);
             this.label3.TabIndex = 5;
             this.label3.Text = "Время рассчета:";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // textBox3
             // 
             this.textBox3.BackColor = System.Drawing.SystemColors.Info;
-            this.textBox3.Location = new System.Drawing.Point(438, 152);
+            this.textBox3.Location = new System.Drawing.Point(438, 204);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(328, 31);
             this.textBox3.TabIndex = 6;
@@ -103,16 +103,37 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 25;
-            this.listBox1.Location = new System.Drawing.Point(38, 214);
+            this.listBox1.Location = new System.Drawing.Point(38, 266);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(728, 179);
             this.listBox1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(526, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Максимальное расстояние (необязательное поле):";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(577, 141);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox4.Size = new System.Drawing.Size(189, 31);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "0";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 478);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label3);
@@ -138,6 +159,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
