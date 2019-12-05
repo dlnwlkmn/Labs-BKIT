9196a7289317e1b45414b833d3d9d107194940c3
Alexey <lesh77906@gmail.com>
2019-12-05 09:26:18 +0300

    Реализован метод вывода информации о сборке

--- laboratory work/lr6_part2/Program.cs
+++ laboratory work/lr6_part2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,16 @@ namespace lr6_part2
 {
     class Program
     {
+        static void AssemblyInformation()
+        {
+            Console.WriteLine("Информация о сборке:");
+            Assembly i = Assembly.GetExecutingAssembly();
+            Console.WriteLine("Полное имя:" + i.FullName);
+            Console.WriteLine("Исполняемый файл:" + i.Location);
+        }
         static void Main(string[] args)
         {
+            AssemblyInformation();
         }
     }
 }
