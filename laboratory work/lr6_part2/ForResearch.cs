8b3215ca9813d2a75b9ca6b8723187e5567b9a93
Alexey <lesh77906@gmail.com>
2019-12-05 09:21:40 +0300

    Создан и реализован Класс для реализации его исследования при помощи рефлексии.

---
+++ laboratory work/lr6_part2/ForResearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr6_part2
+{
+    class ForResearch : IComparable
+    {
+        public ForResearch() { }
+        public ForResearch(int i) { }
+        public ForResearch(string str) { }
+        public int Multiply(int x, int y) { return x * y; }
+        public int Devide(int x, int y) 
+        {
+            if (y != 0) return x / y;
+            else return 0;
+        }
+        [UserAttribute("Описание для property1")]
+        public string first_proper
+        {
+            get { return _first_proper; }
+            set { _first_proper = value; }
+        }
+        private string _first_proper;
+
+        public int second_proper { get; set; }
+
+        [UserAttribute(Description = "Описание для property3")]
+        public double third_proper { get; private set; }
+
+        public int field1;
+        public float field2;
+        public int CompareTo(object obj)
+        {
+            return 0;
+        }
+    }
+}
