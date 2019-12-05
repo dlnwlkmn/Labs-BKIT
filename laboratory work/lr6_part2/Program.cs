5c2829e2fcbb165f0b198df8b46401c01791860a
Alexey <lesh77906@gmail.com>
2019-12-05 09:38:18 +0300

    Реализация метода цветного вывода на экран консоли.

--- laboratory work/lr6_part2/Program.cs
+++ laboratory work/lr6_part2/Program.cs
@@ -76,5 +76,61 @@ namespace lr6_part2
             InvokeMemberInformation();
             Console.ReadKey();
         }
+        static void ColorfulPrint(string outtext, string color)
+        {
+            switch (color)
+            {
+                case "Black":
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                case "Blue":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case "Cyan":
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    break;
+                case "DarkBlue":
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    break;
+                case "DarkCyan":
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    break;
+                case "DarkGray":
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+                case "DarkGreen":
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    break;
+                case "DarkMagenta":
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    break;
+                case "DarkRed":
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    break;
+                case "DarkYellow":
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    break;
+                case "Gray":
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+                case "Green":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case "Magenta":
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    break;
+                case "Red":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case "White":
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case "Yellow":
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+            }
+            Console.WriteLine(outtext);
+            Console.ResetColor();
+        }
     }
 }
