3aebe9e87449aca238b0f5a1a15d0fd3d187151b
Alexey <lesh77906@gmail.com>
2019-12-05 09:35:12 +0300

    Реализация метода вывода информации о типах

--- laboratory work/lr6_part2/Program.cs
+++ laboratory work/lr6_part2/Program.cs
@@ -9,6 +9,7 @@ namespace lr6_part2
 {
     class Program
     {
+        
         static void AssemblyInformation()
         {
             Console.WriteLine("Информация о сборке:");
@@ -16,9 +17,50 @@ namespace lr6_part2
             Console.WriteLine("Полное имя:" + i.FullName);
             Console.WriteLine("Исполняемый файл:" + i.Location);
         }
+        static void TypeInformation()
+        {
+            ForResearch obj = new ForResearch();
+            Type t = obj.GetType();
+
+            Console.WriteLine("\nИнформация о типе:");
+            Console.WriteLine("Тип " + t.FullName + " унаследован от " + t.BaseType.FullName);
+            Console.WriteLine("Пространство имен " + t.Namespace);
+            Console.WriteLine("Распологается в сборке " + t.AssemblyQualifiedName);
+
+            Console.WriteLine("\nКонструкторы:");
+            foreach (var x in t.GetConstructors())
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("\nМетоды:");
+            foreach (var x in t.GetMethods())
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("\nСвойства:");
+            foreach (var x in t.GetProperties())
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("\nПоля данных (public):");
+            foreach (var x in t.GetFields())
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("\nForResearch реализует IComparable -> " +
+            t.GetInterfaces().Contains(typeof(IComparable))
+            );
+        }
+
         static void Main(string[] args)
         {
             AssemblyInformation();
+            TypeInformation();
+            Console.ReadKey();
         }
     }
 }
