69b67ea6c5457a2e7fd305c8b0a0b4e82f8b0e95
Alexey <lesh77906@gmail.com>
2019-12-05 09:22:01 +0300

    Создан и реализован класс-атрибут

---
+++ laboratory work/lr6_part2/UserAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr6_part2
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+
+    class UserAttribute: Attribute
+    {
+        public UserAttribute() { }
+        public UserAttribute(string DescriptionParam)
+        {
+            Description = DescriptionParam;
+        }
+
+        public string Description { get; set; }
+    }
+}
