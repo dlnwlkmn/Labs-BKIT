db8c4818fc52181980926d15cbcb61ef71f36315
Alexey <lesh77906@gmail.com>
2019-12-12 14:31:11 +0300

    Начало разработки ДЗ, вополняется на базе проекта ЛР4 путём модернизации окна

--- laboratory work/lr4_wForms/Form1.Designer.cs
+++ laboratory work/lr4_wForms/Form1.Designer.cs
@@ -45,6 +45,16 @@
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // loadupFileButton
@@ -71,7 +81,7 @@
             // textBoxFileReadTime
             // 
             this.textBoxFileReadTime.BackColor = System.Drawing.SystemColors.Info;
-            this.textBoxFileReadTime.Location = new System.Drawing.Point(420, 127);
+            this.textBoxFileReadTime.Location = new System.Drawing.Point(420, 95);
             this.textBoxFileReadTime.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxFileReadTime.Name = "textBoxFileReadTime";
             this.textBoxFileReadTime.Size = new System.Drawing.Size(678, 31);
@@ -80,7 +90,7 @@
             // textBoxFileReadCount
             // 
             this.textBoxFileReadCount.BackColor = System.Drawing.SystemColors.Info;
-            this.textBoxFileReadCount.Location = new System.Drawing.Point(420, 173);
+            this.textBoxFileReadCount.Location = new System.Drawing.Point(420, 141);
             this.textBoxFileReadCount.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxFileReadCount.Name = "textBoxFileReadCount";
             this.textBoxFileReadCount.Size = new System.Drawing.Size(516, 31);
@@ -89,7 +99,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 133);
+            this.label2.Location = new System.Drawing.Point(32, 101);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(227, 25);
@@ -99,7 +109,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(32, 179);
+            this.label3.Location = new System.Drawing.Point(32, 147);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(354, 25);
@@ -109,7 +119,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(32, 256);
+            this.label4.Location = new System.Drawing.Point(32, 224);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(256, 25);
@@ -118,18 +128,18 @@
             // 
             // searchWord
             // 
-            this.searchWord.Location = new System.Drawing.Point(420, 246);
+            this.searchWord.Location = new System.Drawing.Point(420, 214);
             this.searchWord.Margin = new System.Windows.Forms.Padding(6);
             this.searchWord.Name = "searchWord";
-            this.searchWord.Size = new System.Drawing.Size(516, 31);
+            this.searchWord.Size = new System.Drawing.Size(678, 31);
             this.searchWord.TabIndex = 7;
             // 
             // searchButton
             // 
-            this.searchButton.Location = new System.Drawing.Point(948, 240);
+            this.searchButton.Location = new System.Drawing.Point(829, 381);
             this.searchButton.Margin = new System.Windows.Forms.Padding(6);
             this.searchButton.Name = "searchButton";
-            this.searchButton.Size = new System.Drawing.Size(150, 44);
+            this.searchButton.Size = new System.Drawing.Size(253, 44);
             this.searchButton.TabIndex = 8;
             this.searchButton.Text = "Поиск";
             this.searchButton.UseVisualStyleBackColor = true;
@@ -138,7 +148,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(32, 300);
+            this.label5.Location = new System.Drawing.Point(32, 268);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(222, 25);
@@ -148,7 +158,7 @@
             // textBoxExactTime
             // 
             this.textBoxExactTime.BackColor = System.Drawing.SystemColors.Info;
-            this.textBoxExactTime.Location = new System.Drawing.Point(420, 294);
+            this.textBoxExactTime.Location = new System.Drawing.Point(420, 262);
             this.textBoxExactTime.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxExactTime.Name = "textBoxExactTime";
             this.textBoxExactTime.Size = new System.Drawing.Size(678, 31);
@@ -158,7 +168,7 @@
             // 
             this.listBoxResult.FormattingEnabled = true;
             this.listBoxResult.ItemHeight = 25;
-            this.listBoxResult.Location = new System.Drawing.Point(38, 475);
+            this.listBoxResult.Location = new System.Drawing.Point(37, 628);
             this.listBoxResult.Margin = new System.Windows.Forms.Padding(6);
             this.listBoxResult.Name = "listBoxResult";
             this.listBoxResult.Size = new System.Drawing.Size(1060, 254);
@@ -166,7 +176,7 @@
             // 
             // textButton
             // 
-            this.textButton.Location = new System.Drawing.Point(948, 169);
+            this.textButton.Location = new System.Drawing.Point(948, 137);
             this.textButton.Margin = new System.Windows.Forms.Padding(6);
             this.textButton.Name = "textButton";
             this.textButton.Size = new System.Drawing.Size(150, 44);
@@ -179,18 +189,17 @@
             // 
             this.label6.AutoSize = true;
             this.label6.ForeColor = System.Drawing.SystemColors.ActiveBorder;
-            this.label6.Location = new System.Drawing.Point(32, 343);
+            this.label6.Location = new System.Drawing.Point(32, 311);
             this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(153, 25);
             this.label6.TabIndex = 13;
             this.label6.Text = "Модификация";
-
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(38, 383);
+            this.checkBox1.Location = new System.Drawing.Point(38, 351);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(378, 29);
             this.checkBox1.TabIndex = 14;
@@ -199,7 +208,7 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(576, 421);
+            this.textBox4.Location = new System.Drawing.Point(576, 389);
             this.textBox4.Name = "textBox4";
             this.textBox4.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.textBox4.Size = new System.Drawing.Size(189, 31);
@@ -210,18 +219,123 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(32, 427);
+            this.label7.Location = new System.Drawing.Point(32, 395);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(526, 25);
             this.label7.TabIndex = 15;
             this.label7.Text = "Максимальное расстояние (необязательное поле):";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label8.Location = new System.Drawing.Point(32, 437);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(213, 25);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Паралельный поиск";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(576, 470);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(189, 31);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(32, 476);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(502, 25);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Максимальное расстояние (обязательное поле):";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(270, 526);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox2.Size = new System.Drawing.Size(104, 31);
+            this.textBox2.TabIndex = 21;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(32, 529);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(221, 25);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Количество потоков:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(829, 463);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(253, 44);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Параллельный поиск";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox3.Location = new System.Drawing.Point(780, 526);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox3.Size = new System.Drawing.Size(317, 31);
+            this.textBox3.TabIndex = 24;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(439, 529);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(335, 25);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "Найденное количество потоков:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(33, 581);
+            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(315, 25);
+            this.label12.TabIndex = 26;
+            this.label12.Text = "Время параллельного поиска:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox5.Location = new System.Drawing.Point(421, 575);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(678, 31);
+            this.textBox5.TabIndex = 25;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(1138, 764);
+            this.ClientSize = new System.Drawing.Size(1138, 919);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.checkBox1);
@@ -266,6 +380,16 @@
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
 
