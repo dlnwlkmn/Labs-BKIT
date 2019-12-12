e2eb7ec943597f00d8592f26e16d9a0e13d59e3e
Alexey <lesh77906@gmail.com>
2019-12-12 20:24:44 +0300

    Реализован класс формирования подмасивов

---
+++ laboratory work/lr4_wForms/SabsArrays.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr4_wForms
+{
+    public static class SabsArrays
+    {
+        public static List<MiniMax> DivideSubArrays(int beginIndex, int endIndex, int subArraysCount)
+        {
+            //Результирующий список пар с индексами подмассивов
+            List<MiniMax> result = new List<MiniMax>();
+
+            //Если число элементов в массиве слишком мало для деления то возвращается массив целиком
+            if ((endIndex - beginIndex) <= subArraysCount)
+            {
+                result.Add(new MiniMax(0, (endIndex - beginIndex)));
+            }
+            else
+            {
+                //Размер подмассива
+                int delta = (endIndex - beginIndex) / subArraysCount;
+                //Начало отсчета
+                int currentBegin = beginIndex;
+                //Пока размер подмассива укладывается в оставшуюся последовательность
+                while ((endIndex - currentBegin) >= 2 * delta)
+                {
+                    result.Add(new MiniMax(currentBegin, currentBegin + delta));
+                    currentBegin += delta;
+                }
+                result.Add(new MiniMax(currentBegin, endIndex));
+            }
+            return result;
+        }
+    }
+}
